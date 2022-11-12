1409c3bb246535ea2475ca69ee9a99be4abe038d
Motun Marteen <motunmarteen@gmail.com>
2022-11-12 08:22:56 +0100

    update

---
+++ bitwise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+   int a = 0001000, b = 2, result;
+
+   // <<
+   result = a<<b;
+   printf("a << b = %d \n",result);
+
+   // >>
+   result = a>>b;
+   printf("a >> b = %d \n",result);
+
+   return 0;
+
+}
